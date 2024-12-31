79d7a8f059730fd37190aaececa8cefb2351c187
rajibcsit1 <18192203021@cse.bubt.edu.bd>
2024-12-31 11:24:27 +0600

    Create StripePayment web route and Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StripePaymentController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(StripePaymentController::class)->group(function () {
+    Route::get('stripe', 'stripe');
+    Route::post('stripe', 'stripePost')->name('stripe.post');
 });
