943df6348c08cff67f3e1653f502173348e1295d
rajibcsit1 <18192203021@cse.bubt.edu.bd>
2025-01-01 12:14:54 +0600

    update root url,redirect stripe route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StripePaymentController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('stripe');
 });
 
 Route::controller(StripePaymentController::class)->group(function () {
